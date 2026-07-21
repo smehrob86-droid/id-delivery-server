bd6a4e3ded8a77ca02bd993994256daed3c513a5
smehrob86-droid <smehrob86@gmail.com>
2026-07-21 21:31:50 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ID Delivery server работает!"
+
+@app.route("/test")
+def test():
+    return jsonify({
+        "status": "ok",
+        "message": "Сервер готов"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
